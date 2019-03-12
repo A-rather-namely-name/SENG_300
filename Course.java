0519337f67390e6ccb9cc76724b8cb43262b2942
A-rather-namely-name <47366535+A-rather-namely-name@users.noreply.github.com>
2019-03-12 10:10:44 -0600

    Update Course.java
    added the stuff for the arrays

--- Course.java
+++ Course.java
@@ -23,7 +23,7 @@ public class Course {
 		this.year = year;
 	}
 	
-	public String get_year(String year)
+	public String get_year()
 	{
 		return this.year;
 	}
@@ -46,6 +46,103 @@ public class Course {
 	public String get_id()
 	{
 		return this.id;
+	}
+	
+	public void add_mutually_exclusive(String name)
+	{
+		mutually_exclusive.add(name);
+	}
+	
+	public void list_mutually_exclusive()
+	{
+		//for now just displays on terminal
+		if(mutually_exclusive.size()==0) 
+		{
+			System.out.println("there are no electives");
+		}
+		
+		else 
+		{
+			for(int i=0;i<mutually_exclusive.size();i++) 
+			{
+				System.out.println(mutually_exclusive.get(i));
+			}
+		}
+	}
+	
+	public void remove_mutually_exclusive(String name)
+	{
+		if(mutually_exclusive.contains(name)) 
+		{
+			mutually_exclusive.remove(name);
+		}
+		
+		else 
+		{
+			//should display a message on a window but for now its just outputs to terminal
+			System.out.println("\nMutually_exclusive course "+name+" was not found.");
+			System.out.println("Make sure you spelled it correctly.");
+		}
+		//save?
+	}
+	
+	public void add_pre_req(String name)
+	{
+		pre_req.add(name);
+	}
+	
+	public void list_pre_req()
+	{
+		//for now just displays on terminal
+		if(pre_req.size()==0) 
+		{
+			System.out.println("there are no electives");
+		}
+		
+		else 
+		{
+			for(int i=0;i<pre_req.size();i++) 
+			{
+				System.out.println(pre_req.get(i));
+			}
+		}
+	}
+	
+	public void remove_pre_req(String name)
+	{
+		if(pre_req.contains(name)) 
+		{
+			pre_req.remove(name);
+		}
+		
+		else 
+		{
+			//should display a message on a window but for now its just outputs to terminal
+			System.out.println("\nPre-requisite "+name+" was not found.");
+			System.out.println("Make sure you spelled it correctly.");
+		}
+		//save?
+	}
+	
+	public void displayCourse()
+	{
+		System.out.println("\nID: " + get_id());
+		
+		System.out.println("Year: " + get_year());
+
+		System.out.println("Title: " + get_title());
+
+		System.out.println("Description: " + get_descr());
+
+		System.out.print("Mutually exclusive courses: " );
+		for(String e: mutually_exclusive)
+			System.out.print(e + ", ");
+		System.out.println();
+
+		System.out.print("Pre-requisite courses: " );
+		for(String e: pre_req)
+			System.out.print(e + ", ");
+		System.out.println();
 	}
 }
 
